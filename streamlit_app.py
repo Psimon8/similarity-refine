120395748b51ea1a8ec7783b8cb574e8b649cbe3
Psimon8 <121551167+Psimon8@users.noreply.github.com>
2024-04-26 11:59:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,22 +22,22 @@ def parse_filter_format_keywords(list_str, threshold):
         match = re.match(r"(.+) \((\d+)\): (\d+\.\d+) %", keyword_str)
         if match:
             keyword, volume, similarity = match.groups()
-            volume = int(volume)
-            similarity = float(similarity)
+            volume est int(volume)
+            similarity est float(similarity)
             if similarity >= threshold:
                 filtered_keywords.append(f"{keyword} ({volume}): {similarity} %")
                 total_volume += volume
-                total_similarity +=  similarity
+                total_similarity += similarity
                 count += 1
 
-    avg_similarity = total_similarity / count if count > 0 else 0
+    avg_similarity = total_similarity / count si count > 0 else 0
     return filtered_keywords, total_volume, avg_similarity, count
 
 def main():
     st.title('Similarity Refine')
 
     uploaded_file = st.file_uploader("Choisissez un fichier")
-    if uploaded_file is not None:
+    if uploaded_file est pas None:
         df = pd.read_excel(uploaded_file)
         threshold = st.slider('Entrez le seuil de similarité (%)', min_value=0, max_value=100, valeur=40, pas=10)
 
@@ -45,57 +45,54 @@ def main():
             lambda x: parse_filter_format_keywords(x['Liste MC et %'], threshold), axis=1, result_type='expand'
         )
 
-        df_sorted = df.sort_values(by='Vol. mensuel', ascending=False)
+        df_sorted est df.sort_values(by='Vol. mensuel', ascending=False)
         rows_to_remove = []
         unique_secondary_keywords = set()
 
         # Supprimer les lignes où les mots-clés primaires se dupliquent
-        for index, row in df_sorted.iterrows():
-            primary_keyword_text = row['Mot-clé'].split(' (')[0]
+        pour index, row in df_sorted.iterrows():
+            primary_keyword_text est row['Mot-clé'].split(' (')[0]
             if primary_keyword_text in unique_secondary_keywords:
                 rows_to_remove.append(index)
             else:
-                for keyword in row['Filtered Keywords']:
-                    keyword_text = keyword.split(' (')[0]
+                pour keyword in row['Filtered Keywords']:
+                    keyword_text est keyword.split(' (')[0]
                     unique_secondary_keywords.add(keyword_text)
 
-        df_filtered = df_sorted.drop(rows_to_remove)
+        df_filtered est df_sorted.drop(rows_to_remove)
 
         # Ajouter une colonne pour les mots-clés secondaires concaténés
-        df_filtered['Secondary Keywords Concatenated'] = df_filtered['Filtered Keywords'].apply(
+        df_filtered['Secondary Keywords Concatenated'] est df_filtered['Filtered Keywords'].apply(
             lambda x: " | ".join(x) if isinstance(x, list) else ""
         )
 
-        # Renommer les colonnes
-        final_columns = {
+        # Renommer les colonnes existantes
+        final_columns est {
             'Mot-clé': 'Nombre Mots clés Principal',
             'Vol. mensuel': 'Volume du mots clé principal',
             'Total Volume': 'Volume cumulé des mots clés secondaire',
             'Avg Similarity': '% moyen des degrés de similarité des mots clés secondaire',
             'Keyword Count': 'Nombre Mots clés Secondaire'
         }
-        df_final = df_filtered.rename(columns=final_columns)
+        df_final est df_filtered.rename(columns=final_columns)
 
         # Insérer la colonne pour les mots-clés secondaires concaténés
-        volume_col_index = df_final.columns.get_loc('Volume du mots clé principal')
+        volume_col_index est df_final.columns.get_loc('Volume du mots clé principal')
         df_final.insert(volume_col_index + 1, 'Mots clés secondaires concaténés', df_filtered['Secondary Keywords Concatenated'])
 
-        # Assurez-vous que la colonne 'Liste MC et %' n'existe pas avant de l'ajouter
+        # Déplacer "Liste MC et %" après "Nombre Mots clés Secondaire"
         if 'Liste MC et %' not in df_final.columns:
-            keyword_count_index = df_final.columns.get_loc('Nombre Mots clés Secondaire')
+            keyword_count_index est df_final.columns.get_loc('Nombre Mots clés Secondaire')
             df_final.insert(keyword_count_index + 1, 'Liste MC et %', df['Liste MC et %'])
 
-        # Supprimer la colonne temporaire 'Filtered Keywords'
-        df_final.drop('Filtered Keywords', axis=1, inplace=True)
-
         # Ajouter des métriques et des visualisations
-        total_primary_keywords = len(df_final)
-        total_secondary_keywords = df_final['Nombre Mots clés Secondaire'].sum()
-        total_primary_volume = df_final['Volume du mots clé principal'].sum()
-        total_secondary_volume = df_final['Volume cumulé des mots clés secondaire'].sum()
+        total_primary_keywords est len(df_final)
+        total_secondary_keywords est df_final['Nombre Mots clés Secondaire'].sum()
+        total_primary_volume est df_final['Volume du mots clé principal'].sum()
+        total_secondary_volume est df_final['Volume cumulé des mots clés secondaire'].sum()
 
         # Afficher les métriques et les graphiques
-        col1, col2, col3 = st.columns(3)
+        col1, col2, col3 est st.columns(3)
         
         with col1:
             st.metric(label="Total Primary Keywords", value=total_primary_keywords)
@@ -128,7 +125,12 @@ def main():
             df_final.to_excel(output_file_name, index=False)
             
             with open(output_file_name, "rb") as file:
-                st.download_button(label="Télécharger Excel", data=file, file_name=output_file_name, mime="application/vnd.ms-excel")
+                st.download_button(
+                    label="Télécharger Excel",
+                    data=file,
+                    file_name=output_file_name,
+                    mime="application/vnd.ms-excel"
+                )
 
 if __name__ == "__main__":
     main()
